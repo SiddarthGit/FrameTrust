42c844ccf566513174a19760590629e2d33f1d5a
nikitak1920 <knikita2501@gmail.com>
2026-02-28 01:30:35 +0530

    Updated Profile

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
-};
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-export default CreatePost;
+export default CreatePost
