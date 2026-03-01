52bedbfd5242b642919e52a4e4d76a51f5e2519d
Siddharth Nair <siddharthnair@SiddharthMacBookPro.local>
2026-03-01 18:29:40 +0530

    Improving DB connectivity and adding user model

--- server/server.js
+++ server/server.js
@@ -1,20 +1,16 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// 👇 explicitly load .env from THIS folder
-dotenv.config({ path: path.join(__dirname, ".env") });
-
-console.log("Mongo URL:", process.env.MONGODB_URL); // should NOT be undefined
-
-import connectDB from "./configs/db.js";
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
+
+dotenv.config();
+
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
