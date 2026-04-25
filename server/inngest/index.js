e26cac6a6c7be91e7ec2e2ef5247b46751dee5d7
nikitak1920 <knikita2501@gmail.com>
2026-04-26 04:28:37 +0530

    Completed Backend

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import sendEmail from "../configs/nodeMailer.js";
 import Story from "../models/Story.js";
+import Message from "../models/Message.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "frametrust-app" });
@@ -84,7 +85,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
       <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981;">here</a> to accept or reject the request</p>
       <br/>
       <p>Thanks,<br/>FrameTrust - Stay Connected</p>
-      </div>`
+      </div>`;
 
       await sendEmail({
         to: connection.to_user_id.email,
@@ -110,7 +111,7 @@ const sendNewConnectionRequestReminder = inngest.createFunction(
       <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981;">here</a> to accept or reject the request</p>
       <br/>
       <p>Thanks,<br/>FrameTrust - Stay Connected</p>
-      </div>`
+      </div>`;
 
       await sendEmail({
         to: connection.to_user_id.email,
@@ -139,11 +140,47 @@ const deleteStory = inngest.createFunction(
   }
 )
 
+const sendNotificationOfUnseenMessages = inngest.createFunction(
+  {id: "send-unseen-messages-notification"},
+  {cron: "TZ=America/New_York 0 9 * * *"}, //Everyday at 9 AM
+  async ({step}) => {
+    const messages = await Message.find({seen: false}).populate('to_user_id');
+    const unseenCount = {}
+
+    messages.map(message=> {
+      unseenCount[message.to_user_id] = (unseenCount[message.to_user_id] || 0) + 1;
+    })
+
+    for (const userId in unseenCount) {
+      const user = await User.findById(userId);
+
+      const subject = `📬 You have ${unseenCount[userId]} unseen messages`;
+
+      const body = `
+      <div style="font-family: Arial, sans-serif; padding: 20px;">
+        <h2>Hi ${user.full_name},</h2>
+        <p>You have ${unseenCount[userId]} unseen messages</p>
+        <p>Click <a href="${process.env.FRONTEND_URL}/messages" style="color: #10b981;">here</a> to view them</p>
+        <br/>
+        <p>Thanks,<br/>FrameTrust - Stay Connected</p>
+      </div>`;
+
+      await sendEmail({
+        to: user.email,
+        subject,
+        body
+      })
+    }
+    return {message: "Notification sent."}
+  }
+)
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
   syncUserCreation, 
   syncUserUpdation, 
   syncUserDeletion,
   sendNewConnectionRequestReminder,
-  deleteStory
+  deleteStory,
+  sendNotificationOfUnseenMessages
 ];
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
 import postRouter from "./routes/postRoutes.js";
 import storyRouter from "./routes/storyRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
@@ -29,6 +30,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user", userRouter);
 app.use('/api/post', postRouter)
 app.use('/api/story', storyRouter)
+app.use('/api/message', messageRouter)
 
 const PORT = process.env.PORT || 4000;
 
